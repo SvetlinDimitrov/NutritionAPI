7e26d66174be6c6da981f2b5f0b1ba793c51cf07
Svetlin DImitrov <prolama6@gmail.com>
2023-08-15 21:12:11 +0300

    create vitamin entity

---
+++ nutritionAPI/src/test/java/com/example/nutritionapi/NutritionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nutritionapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutritionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
