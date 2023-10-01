9ebedf9149e504a8689f9cecb3b312ecdd422c7a
Svetlin DImitrov <prolama6@gmail.com>
2023-10-01 11:06:51 +0300

    prepare for docker

--- nutritionAPI/src/main/java/com/example/nutritionapi/NutritionApiApplication.java
+++ nutritionAPI/src/main/java/com/example/nutritionapi/NutritionApiApplication.java
@@ -1,11 +1,15 @@
 package com.example.nutritionapi;
 
+import com.example.nutritionapi.domain.constants.enums.EnvVariables;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
+@EnableConfigurationProperties(EnvVariables.class)
 public class NutritionApiApplication {
 
 	public static void main(String[] args) {
--- nutritionAPI/src/main/java/com/example/nutritionapi/config/security/JwtUtil.java
+++ nutritionAPI/src/main/java/com/example/nutritionapi/config/security/JwtUtil.java
@@ -2,11 +2,10 @@ package com.example.nutritionapi.config.security;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import com.example.nutritionapi.domain.constants.enums.EnvVariables;
 import com.example.nutritionapi.domain.entity.UserEntity;
 import com.example.nutritionapi.service.UserServiceImp;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -20,35 +19,34 @@ import java.util.List;
 @Component
 public class JwtUtil {
 
-    @Value("${security.api-secret-key}")
-    private String secret;
-
+    private final EnvVariables envVariables;
     private final UserServiceImp userService;
 
-    public JwtUtil(UserServiceImp userService) {
+    public JwtUtil(EnvVariables envVariables, UserServiceImp userService) {
+        this.envVariables = envVariables;
         this.userService = userService;
     }
 
-    public String createJwtToken(Long userId){
+    public String createJwtToken(Long userId) {
         return JWT.create()
                 .withSubject(String.valueOf(userId))
                 .withExpiresAt(Instant.now().plus(Duration.of(30, ChronoUnit.MINUTES)))
-                .sign(Algorithm.HMAC256(secret));
+                .sign(Algorithm.HMAC256(envVariables.apiSecretKey()));
     }
 
-    public DecodedJWT decodeToken(String token){
-        return JWT.require(Algorithm.HMAC256(secret))
-                    .build()
-                    .verify(token);
+    public DecodedJWT decodeToken(String token) {
+        return JWT.require(Algorithm.HMAC256(envVariables.apiSecretKey()))
+                .build()
+                .verify(token);
 
 
     }
 
-    public UserDetails convert(DecodedJWT token){
+    public UserDetails convert(DecodedJWT token) {
         UserEntity user = userService.findById(Long.parseLong(token.getSubject()));
-        return new User(user.getEmail() ,
+        return new User(user.getEmail(),
                 user.getPassword(),
-                List.of(new SimpleGrantedAuthority("ROLE_"+user.getUserDetails().name())));
+                List.of(new SimpleGrantedAuthority("ROLE_" + user.getUserDetails().name())));
 
     }
 }
