871685616e2913990de0e28d5c81e5ed211b8753
Светлин Димитров <dimitrovmail99@gmail.com>
2024-04-09 11:08:41 +0300

    test workflow

--- reactive_nutritionAPI/src/main/java/com/api/reactive_nutritionapi/ReactiveNutritionApiApplication.java
+++ reactive_nutritionAPI/src/main/java/com/api/reactive_nutritionapi/ReactiveNutritionApiApplication.java
@@ -10,7 +10,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 public class ReactiveNutritionApiApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(ReactiveNutritionApiApplication.class, args);
 	}
 
